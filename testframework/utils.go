cc3c4f2d7c583a63e0ea3323b502916874a76afb
Patrick Cowland <patrickcowland@pingidentity.com>
2025-06-04 12:34:07 +0100

    Refactor randomString to use crypto/rand for secure random string generation

--- testframework/utils.go
+++ testframework/utils.go
@@ -3,7 +3,8 @@
 package testframework
 
 import (
-	"math/rand/v2"
+	"crypto/rand"
+	"math/big"
 )
 
 // randomString generates a random string of a given length.
@@ -11,7 +12,13 @@ func randomString(length int) string {
 	const charset = "abcdefghijklmnopqrstuvwxyz"
 	b := make([]rune, length)
 	for i := range b {
-		b[i] = rune(charset[rand.IntN(len(charset))])
+		// Generate a random index within the charset
+		maxBigInt := big.NewInt(int64(len(charset)))
+		n, err := rand.Int(rand.Reader, maxBigInt)
+		if err != nil {
+			panic(err) // In a utility function, we panic on errors
+		}
+		b[i] = rune(charset[n.Int64()])
 	}
 	return string(b)
 }
